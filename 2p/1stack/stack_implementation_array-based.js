5f8488969c2a6686ab0449f7aff16908fd680c26
RogerJIR <riglesias0@ucol.mx>
2021-11-03 19:15:53 -0600

    Corrección de ejercicios

--- 2p/1stack/stack_implementation_array-based.js
+++ 2p/1stack/stack_implementation_array-based.js
@@ -9,8 +9,9 @@
 //PROBLEM 1
 //Stack implementation (array-based)
 
+//Estructura de la pila
 function Stack() {
-    this.dataStore = [];
+    this.dataStore = []; //Estructura basada en arreglos
     this.top = 0;
     this.push = push;
     this.pop = pop;
@@ -18,7 +19,7 @@ function Stack() {
     this.clear = clear;
     this.length = length;
 }
-
+//funciones de la estructura de la pila
 function push(element) {
     this.dataStore[this.top++] = element;
 }
@@ -38,25 +39,27 @@ function clear() {
 function length() {
     return this.top;
 }
-
+//Definimos una nueva pila
 var nombres = new Stack();
-
+//Ingresar elementos a la pila
 nombres.push("Jesús");
 nombres.push("Fernanda");
 nombres.push("Vladimir");
-console.log("Longitud: " + nombres.length());
-console.log(nombres.peek());
-
-var popped = nombres.pop();
+console.log("Longitud: " + nombres.length()); //Imprimir la longitud de la píla
+console.log(nombres.peek()); //Mostrar el último elemento de la pila
+//Sacar elemento (último) de la pila
+var popped = nombres.pop(); 
 console.log("El elemento que sale es: " + popped);
-console.log(nombres.peek());
 
-nombres.push("Rogelio");
-console.log(nombres.peek());
+console.log(nombres.peek()); //Mostrar el último elemento de la pila
 
-nombres.clear();
-console.log("Longitud: " + nombres.length());
-console.log(nombres.peek());
+nombres.push("Rogelio"); //Ingresar elemento a la pila
+console.log(nombres.peek()); //Mostrar el último elemento de la pila
 
-nombres.push("María");
-console.log(nombres.peek());
+nombres.clear(); //Limpiar la pila
+
+console.log("Longitud: " + nombres.length()); //Imprimir la longitud de la píla
+console.log(nombres.peek()); //Mostrar el último elemento de la pila
+
+nombres.push("María"); //Ingresar elementos a la pila
+console.log(nombres.peek()); //Mostrar el último elemento de la pila
